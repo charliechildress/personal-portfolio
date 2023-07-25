1a7841b0e39e5fc2c9f854d46e8a39311298ca14
charliechildress <charlie.t.childress.24@dartmouth.edu>
2023-07-25 13:57:31 -0400

    video demo

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,80 +1,80 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { motion } from "framer-motion";
-import { slideInLeft } from "../animations";
+import { motion } from 'framer-motion';
+import { slideInLeft } from '../animations';
 
-import { projects } from "../constants";
-import VideoDemo from "./VideoDemo";
+import { projects } from '../constants';
+import VideoDemo from './VideoDemo';
 
-import GitHubIcon from "@mui/icons-material/GitHub";
-import PersonalVideoIcon from "@mui/icons-material/PersonalVideo";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PersonalVideoIcon from '@mui/icons-material/PersonalVideo';
 
 const ProjectCards = () => {
-	return (
-		<>
-			{projects.map((project) => {
-				return (
-					<motion.div
-						initial="hidden"
-						whileInView="visible"
-						viewport={{ once: true }}
-						variants={slideInLeft}
-						className="flex pb-20"
-					>
-						<div className="flex flex-row relative pl-10  bg-card-bg w-full min-h-[250px] shadow-[20px_0px_100px_rgba(49,49,255,0.7)] border-4 rounded-lg border-card-border">
-							<div className="flex-0 w-1/2 pt-7">
-								<p className="text-4xl font-bold text-card-text">
-									{project.projectName}
-								</p>
-								<p className="pt-5 text-xl font-bold text-card-text">
-									{project.organization}
-								</p>
-								<p className="pt-4 text-md font-semibold text-card-text">
-									{project.information}
-								</p>
-								<p className="flex flex-row flex-auto pt-8 text-card-text">
-									{project.languages.map((language) => (
-										<p
-											className="text-card-text bg-red-500 rounded-md px-1 mr-2"
-											key={language}
-										>
-											{language}{" "}
-										</p>
-									))}{" "}
-									{project.code}
-								</p>
-								<div className="flex flex-row">
-									<a
-										href="https://github.com"
-										target="_blank"
-										rel="noopener noreferrer"
-										aria-label="icon"
-										className="mt-8 p-2 flex flex-row w-1/4 bg-red-500 rounded-md"
-									>
-										<GitHubIcon />
-										<p className="text-card-text pl-2">Look at code</p>
-									</a>
-									<a
-										href="https://google.com"
-										target="_blank"
-										rel="noopener noreferrer"
-										aria-label="icon"
-										className="mt-8 p-2 ml-8 flex flex-row w-1/4 bg-red-500 rounded-md"
-									>
-										<PersonalVideoIcon />
-										<p className="text-card-text pl-2">Visit site</p>
-									</a>
-								</div>
-							</div>
-							<div className="flex-1 scale-[0.80]">
-								<VideoDemo />
-							</div>
-						</div>
-					</motion.div>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      {projects.map((project) => {
+        return (
+          <motion.div
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            variants={slideInLeft}
+            className='flex pb-20'
+          >
+            <div className='flex flex-row relative pl-10  bg-card-bg w-full min-h-[250px] shadow-[20px_0px_100px_rgba(49,49,255,0.7)] border-4 rounded-lg border-card-border'>
+              <div className='flex-0 w-1/2 pt-7'>
+                <p className='text-4xl font-bold text-card-text'>
+                  {project.projectName}
+                </p>
+                <p className='pt-5 text-xl font-bold text-card-text'>
+                  {project.organization}
+                </p>
+                <p className='pt-4 text-md font-semibold text-card-text'>
+                  {project.information}
+                </p>
+                <p className='flex flex-row flex-auto pt-8 text-card-text'>
+                  {project.languages.map((language) => (
+                    <p
+                      className='text-card-text bg-red-500 rounded-md px-1 mr-2'
+                      key={language}
+                    >
+                      {language}{' '}
+                    </p>
+                  ))}{' '}
+                  {project.code}
+                </p>
+                <div className='flex flex-row'>
+                  <a
+                    href='https://github.com'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='icon'
+                    className='mt-8 p-2 flex flex-row w-1/4 bg-red-500 rounded-md'
+                  >
+                    <GitHubIcon />
+                    <p className='text-card-text pl-2'>Look at code</p>
+                  </a>
+                  <a
+                    href='https://google.com'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='icon'
+                    className='mt-8 p-2 ml-8 flex flex-row w-1/4 bg-red-500 rounded-md'
+                  >
+                    <PersonalVideoIcon />
+                    <p className='text-card-text pl-2'>Visit site</p>
+                  </a>
+                </div>
+              </div>
+              <div className='flex-1 scale-[0.80] '>
+                <VideoDemo />
+              </div>
+            </div>
+          </motion.div>
+        );
+      })}
+    </>
+  );
 };
 
 export default ProjectCards;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,83 +3,83 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			screens: {
-				xs: { max: "475px" },
-				sm: { min: "476px", max: "767px" },
-				// => @media (min-width: 640px and max-width: 767px) { ... }
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: { max: '475px' },
+        sm: { min: '476px', max: '767px' },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
 
-				md: { min: "768px", max: "1023px" },
-				// => @media (min-width: 768px and max-width: 1023px) { ... }
+        md: { min: '768px', max: '1023px' },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-				lg: { min: "1024px", max: "1279px" },
-				// => @media (min-width: 1024px and max-width: 1279px) { ... }
+        lg: { min: '1024px', max: '1279px' },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-				xl: { min: "1280px", max: "1535px" },
-				// => @media (min-width: 1280px and max-width: 1535px) { ... }
+        xl: { min: '1280px', max: '1535px' },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-				"2xl": { min: "1536px", max: "1700px" },
-				// => @media (min-width: 1536px) { ... }
-				"3xl": { min: "1701px" },
-			},
-			fontFamily: {
-				neon: "Neon",
-				alegreya: "Alegreya",
-				"alegreya-italic": "Alegreya-Italic",
-			},
-			textShadow: {
-				neon: "4.5px 6px 16px #FF3131",
-				neon2: "16px 12px 32px #FF3131",
-			},
-			colors: {
-				"neon-red": "#DC143C",
-				"neon-blue": "#00FFFF",
-				"dark-bg": "#000000",
-				"dark-text": "#F5F5F5",
-				"card-text": "#FFFFFF",
-				"card-bg": "#6A5ACD",
-				"card-border": "#6699FF",
-			},
-			keyframes: {
-				flicker: {
-					"0%, 25%, 50%, 75%, 100%": {
-						textShadow: "4.5px 6px 16px #FF3131",
-						opacity: "1",
-					},
-					"12.5%, 37.5%, 62.5%, 87.5%": {
-						textShadow: "-4px 12px 32px #FF3131",
-						opacity: "0.7",
-					},
-				},
-				lineFlicker: {
-					"0%, 25%, 50%, 75%, 100%": {
-						dropShadow: "[5px_5px_10px_rgba(255,49,49,0.7)]",
-						opacity: "1",
-					},
-					"12.5%, 37.5%, 62.5%, 87.5%": {
-						dropShadow: "[0px_0px_10px_rgba(255,49,49,0.7)]",
-						opacity: "0.7",
-					},
-				},
-			},
-			animation: {
-				flicker: "flicker 3.75s infinite",
-				lineFlicker: "lineFlicker 3.75s infinite",
-			},
-		},
-	},
-	plugins: [
-		plugin(function ({ matchUtilities, theme }) {
-			matchUtilities(
-				{
-					"text-shadow": (value) => ({
-						textShadow: value,
-					}),
-				},
-				{ values: theme("textShadow") }
-			);
-		}),
-	],
+        '2xl': { min: '1536px', max: '1700px' },
+        // => @media (min-width: 1536px) { ... }
+        '3xl': { min: '1701px' },
+      },
+      fontFamily: {
+        neon: 'Neon',
+        alegreya: 'Alegreya',
+        'alegreya-italic': 'Alegreya-Italic',
+      },
+      textShadow: {
+        neon: '4.5px 6px 16px #FF3131',
+        neon2: '16px 12px 32px #FF3131',
+      },
+      colors: {
+        'neon-red': '#DC143C',
+        'neon-blue': '#00FFFF',
+        'dark-bg': '#000000',
+        'dark-text': '#F5F5F5',
+        'card-text': '#FFFFFF',
+        'card-bg': 'hsl(249,55%,56%)',
+        'card-border': '#6699FF',
+      },
+      keyframes: {
+        flicker: {
+          '0%, 25%, 50%, 75%, 100%': {
+            textShadow: '4.5px 6px 16px #FF3131',
+            opacity: '1',
+          },
+          '12.5%, 37.5%, 62.5%, 87.5%': {
+            textShadow: '-4px 12px 32px #FF3131',
+            opacity: '0.7',
+          },
+        },
+        lineFlicker: {
+          '0%, 25%, 50%, 75%, 100%': {
+            dropShadow: '[5px_5px_10px_rgba(255,49,49,0.7)]',
+            opacity: '1',
+          },
+          '12.5%, 37.5%, 62.5%, 87.5%': {
+            dropShadow: '[0px_0px_10px_rgba(255,49,49,0.7)]',
+            opacity: '0.7',
+          },
+        },
+      },
+      animation: {
+        flicker: 'flicker 3.75s infinite',
+        lineFlicker: 'lineFlicker 3.75s infinite',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      );
+    }),
+  ],
 };
