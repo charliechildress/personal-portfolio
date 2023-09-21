0d2e88d01ad2a8b2b19fd24c9e28a7c8458a69bd
Charlie Childress <Charlie.t.childress.24@dartmouth.edu>
2023-09-21 15:08:15 -0400

    fix website url

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,61 +12,60 @@ const introduction = [
 const typeWords = ['Hello, World!', 'Welcome to my website!'];
 
 const projects = [
-  {
-    projectName: 'GreenBox Delivery App',
-    organization: 'GreenBox Storage',
-    information:
-      'User friendly app that allows for the management and logging of inventory for thousands of storage dropoffs and pickups across dozens of locations.',
-    languages: ['HTML', 'CSS', 'JavaScript', 'React Native'],
-    code: 'Contact to view code',
-    demo: greenboxApp,
-  },
-  {
-    projectName: 'Charliechildress.me',
-    organization: 'Independent',
-    information:
-      'Personal website to apply my skills in web development and UI/UX design. Hope you enjoy it!',
-    languages: ['HTML', 'CSS', 'JavaScript', 'React'],
-    code: 'https://github.com/charliechildress/personal-portfolio.git',
-    link: ' ',
-    demo: personalWebsite,
-  },
-  {
-    projectName: 'GreenBox Dashboard',
-    organization: 'GreenBox Storage',
-    information:
-      'Dashboard that provides real-time insights and centralized control for order management, customer data, and key administrative tasks.',
-    languages: ['HTML', 'CSS', 'TypeScript', 'React'],
-    code: 'Contact to view code',
-    demo: greenboxWebsite,
-  },
-  {
-    projectName: 'Tiny Search Engine',
-    organization: 'Dartmouth Computer Science Department, CS50',
-    information:
-      'Mini version of the Google search engine. Crawls through web pages, indexes word occurrences, and returns the best URLs based on word relevance to the query.',
-    languages: ['C'],
-    code: 'https://github.com/charliechildress/Mini-Search.git',
-    demo: tseDemo,
-  },
-  {
-    projectName: 'Nuggets',
-    organization: 'Dartmouth Computer Science Department, CS50',
-    information:
-      'A multi-player exploration game, Nuggets, in which a game server maintains all game state, and one or more game clients display the game to a user. The object of the game is to collect more gold nuggets than any other player.',
-    languages: ['C'],
-    code: 'Contact to view code',
-    demo: nuggetsDemo,
-  },
-  {
-    projectName: 'Collaborative Sketch Editor',
-    organization: 'Dartmouth Computer Science Department, CS10',
-    information:
-      'Collaborative graphical editor that allows multiple simultaneous editors on the same document. The server can handle multiple objects at a time, and clients can draw rectangles, line segments, elipses and freehand shapes.',
-    languages: ['Java'],
-    code: 'https://github.com/charliechildress/CS10/tree/main/PSET6',
-    demo: cse,
-  },
+	{
+		projectName: "GreenBox Delivery App",
+		organization: "GreenBox Storage",
+		information:
+			"User friendly app that allows for the management and logging of inventory for thousands of storage dropoffs and pickups across dozens of locations.",
+		languages: ["HTML", "CSS", "JavaScript", "React Native"],
+		code: "Contact to view code",
+		demo: greenboxApp,
+	},
+	{
+		projectName: "Charliechildress.com",
+		organization: "Independent",
+		information:
+			"Personal website to apply my skills in web development and UI/UX design. Hope you enjoy it!",
+		languages: ["HTML", "CSS", "JavaScript", "React"],
+		code: "https://github.com/charliechildress/personal-portfolio.git",
+		demo: personalWebsite,
+	},
+	{
+		projectName: "GreenBox Dashboard",
+		organization: "GreenBox Storage",
+		information:
+			"Dashboard that provides real-time insights and centralized control for order management, customer data, and key administrative tasks.",
+		languages: ["HTML", "CSS", "TypeScript", "React"],
+		code: "Contact to view code",
+		demo: greenboxWebsite,
+	},
+	{
+		projectName: "Tiny Search Engine",
+		organization: "Dartmouth Computer Science Department, CS50",
+		information:
+			"Mini version of the Google search engine. Crawls through web pages, indexes word occurrences, and returns the best URLs based on word relevance to the query.",
+		languages: ["C"],
+		code: "https://github.com/charliechildress/Mini-Search.git",
+		demo: tseDemo,
+	},
+	{
+		projectName: "Nuggets",
+		organization: "Dartmouth Computer Science Department, CS50",
+		information:
+			"A multi-player exploration game, Nuggets, in which a game server maintains all game state, and one or more game clients display the game to a user. The object of the game is to collect more gold nuggets than any other player.",
+		languages: ["C"],
+		code: "Contact to view code",
+		demo: nuggetsDemo,
+	},
+	{
+		projectName: "Collaborative Sketch Editor",
+		organization: "Dartmouth Computer Science Department, CS10",
+		information:
+			"Collaborative graphical editor that allows multiple simultaneous editors on the same document. The server can handle multiple objects at a time, and clients can draw rectangles, line segments, elipses and freehand shapes.",
+		languages: ["Java"],
+		code: "https://github.com/charliechildress/CS10/tree/main/PSET6",
+		demo: cse,
+	},
 ];
 
 const skills = [
